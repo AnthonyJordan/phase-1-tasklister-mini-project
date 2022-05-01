bb631da1153c9a18ddc807b7ed9f5dfb31f53bb1
Anthony <Kain235@gmail.com>
2022-05-01 14:46:13 -0400

    Added taks and delete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+  })
+
+  function buildToDo(toDo) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${toDo} `
+    p.appendChild(btn)
+    document.getElementById('tasks').appendChild(p)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
+
 });
